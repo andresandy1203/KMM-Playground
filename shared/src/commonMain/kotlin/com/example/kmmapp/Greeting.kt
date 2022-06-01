bcd5129d951c817a9a5fb0853df06b662f2d975b
andres gil <andres.gil120397@gmail.com>
2022-06-01 13:46:34 -0400

    Part 1 basic hello world

---
+++ shared/src/commonMain/kotlin/com/example/kmmapp/Greeting.kt
@@ -0,0 +1,7 @@
+package com.example.kmmapp
+
+class Greeting {
+    fun greeting(): String {
+        return "Hello this is Harry the Henry, ${Platform().platform} This is our KMM Shared Library!"
+    }
+}
